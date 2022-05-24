1ce1de544b7b58666c197667e98067d4b43a74f7
saladaczia <saladaczia@icloud.com>
2022-05-24 12:09:06 +0200

    Initial Commit

---
+++ CalculatorB2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorB2B
+//
+//  Created by Maciej Sołoducha on 24/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
